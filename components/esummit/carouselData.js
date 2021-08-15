e6e055f9bf349f0f7d5d8817f701fa067691b1fc
Akshat Khaitan <khaitanakshat40@gmail.com>
2021-08-15 23:02:20 +0530

    first commit

---
+++ components/esummit/carouselData.js
@@ -0,0 +1,42 @@
+import sanchit from "../../assets/images/tech/sanchit.jpeg";
+import gr1 from "../../assets/images/Carousel/GR/gr1.jpg";
+import gr2 from "../../assets/images/Carousel/GR/gr2.jpg";
+import gr3 from "../../assets/images/Carousel/GR/gr3.jpg";
+import gr4 from "../../assets/images/Carousel/GR/gr4.jpg";
+import gr5 from "../../assets/images/Carousel/GR/gr5.jpg";
+import es1 from "../../assets/images/Carousel/ESummit/es1.jpg";
+import es2 from "../../assets/images/Carousel/ESummit/es2.jpg";
+import es3 from "../../assets/images/Carousel/ESummit/es3.jpg";
+import es4 from "../../assets/images/Carousel/ESummit/es4.jpg";
+import es5 from "../../assets/images/Carousel/ESummit/es5.jpg";
+import bp1 from "../../assets/images/Carousel/BP/bp1.png"
+import bp2 from "../../assets/images/Carousel/BP/bp2.png"
+import bp3 from "../../assets/images/Carousel/BP/bp3.png"
+import bp4 from "../../assets/images/Carousel/BP/bp4.png"
+import bp5 from "../../assets/images/Carousel/BP/bp5.png"
+import bp6 from "../../assets/images/Carousel/BP/bp6.png"
+import se1 from "../../assets/images/Carousel/Startup Expo/se1.jpg";
+import se2 from "../../assets/images/Carousel/Startup Expo/se2.jpg";
+import se3 from "../../assets/images/Carousel/Startup Expo/se3.jpg";
+import se4 from "../../assets/images/Carousel/Startup Expo/se4.jpg";
+import se5 from "../../assets/images/Carousel/Startup Expo/se5.jpg";
+import se6 from "../../assets/images/Carousel/Startup Expo/se6.jpg";
+import se7 from "../../assets/images/Carousel/Startup Expo/se7.jpg";
+export const carouselData = [
+  {
+    image: es1,
+  },
+  {
+    image: es2,
+  },
+  {
+    image: es3,
+  },
+
+  {
+    image: es4,
+  },
+  {
+    image: es5,
+  },
+];
---
+++ components/eventsdata.js
@@ -0,0 +1,104 @@
+
+import img1 from "../assets/images/address.png";
+import img2 from "../assets/images/arrow.png";
+import img3 from "../assets/images/DV.png";
+import img4 from "../assets/images/email.png";
+import img5 from "../assets/images/phone.png";
+import gr1 from "../assets/images/Carousel/GR/gr1.jpg"
+import gr2 from "../assets/images/Carousel/GR/gr2.jpg"
+import gr3 from "../assets/images/Carousel/GR/gr3.jpg"
+import gr4 from "../assets/images/Carousel/GR/gr4.jpg"
+import gr5 from "../assets/images/Carousel/GR/gr5.jpg"
+import bp1 from "../assets/images/Carousel/BP/bp1.png";
+import bp2 from "../assets/images/Carousel/BP/bp2.png";
+import bp3 from "../assets/images/Carousel/BP/bp3.png";
+import bp4 from "../assets/images/Carousel/BP/bp4.png";
+import bp5 from "../assets/images/Carousel/BP/bp5.png";
+import bp6 from "../assets/images/Carousel/BP/bp6.png";
+import se1 from "../assets/images/Carousel/Startup Expo/se1.jpg"
+import se2 from "../assets/images/Carousel/Startup Expo/se2.jpg"
+import se3 from "../assets/images/Carousel/Startup Expo/se3.jpg"
+import se4 from "../assets/images/Carousel/Startup Expo/se4.jpg"
+import se5 from "../assets/images/Carousel/Startup Expo/se5.jpg"
+import se6 from "../assets/images/Carousel/Startup Expo/se6.jpg"
+import se7 from "../assets/images/Carousel/Startup Expo/se7.jpg"
+
+import sanchit from "../assets/images/tech/sanchit.jpeg"
+ const eventdata = [
+  {
+    event: "groundreality",
+    heading: "Ground Reality",
+    caption: "Idea wont become reality unitil it is tested",
+    carouselImages: [
+      {
+        image: gr1,
+      },
+      {
+        image: gr2,
+      },
+      {
+        image: gr3,
+      },
+      {
+        image: gr4,
+      },
+      {
+        image: gr5,
+      },
+    ],
+  },
+  {
+    event: "beyondprofits",
+    heading: "Beyond Profits",
+    caption: "A head for business, a heart for the world",
+    carouselImages: [
+      {
+        image: bp1,
+      },
+      {
+        image: bp2,
+      },
+      {
+        image: bp3,
+      },
+      {
+        image: bp4,
+      },
+      {
+        image: bp5,
+      },
+      {
+        image: bp6,
+      },
+    ],
+  },
+  {
+    event: "startupexpo",
+    heading: "Startup Expo",
+    caption: "Ideas require feedback to improve",
+    carouselImages: [
+      {
+        image: se1,
+      },
+      {
+        image: se2,
+      },
+      {
+        image: se3,
+      },
+      {
+        image: se4,
+      },
+      {
+        image: se5,
+      },
+      {
+        image: se6,
+      },
+      {
+        image: se7,
+      },
+    ],
+  },
+];
+export default eventdata;
